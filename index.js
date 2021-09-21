32afc084618e1ad59883ae25f0ac5e94bb334150
CodeAdi-NaskoTV <65488624+CodeAdi-NaskoTV@users.noreply.github.com>
2021-09-22 00:02:37 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+
+<head>
+  <!--      CSS       -->
+
+  <link href="css/style.css" rel="stylesheet" />
+
+  <!--      META       -->
+
+  <title>Re4e Bot</title>
+  
+  <meta charset="utf-8" />
+  <meta name="description" content="Re4e bot is better bot" />
+  <meta name="keywords" content="re4e" />
+  <meta
+    name="author"
+        content="https://cdn.discordapp.com/icons/878341201399521351/30a501ea20ba724492fc6b9a90844f8f.webp?size=1024"
+  />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta property="og:title" content="Re4e Bot" />
+  <meta property="og:description" content="Re4e bot is better bot" />
+  <meta property="og:url" content="https://discord.gg/sdAN2PrU" />
+</head>
+ 
+        <link rel = "icon" href = 
+"https://cdn.discordapp.com/icons/878341201399521351/30a501ea20ba724492fc6b9a90844f8f.webp?size=1024" 
+        type = "image/x-icon">
+
+
+<nav id="nav">
+  <div id="navtext-div">
+    <a href="application.html" id="navtext">Application</a>
+
+    <a id="navtext">Home</a>
+
+    <a href="commands" id="navtext">Commands</a>
+    
+<a href="https://discord.com/oauth2/authorize?client_id=878298215479410749&permissions=8&scope=bot" id="navtext">Add to Server</a> 
+  </div>
+</nav>
+
+<body>
+  <img
+    id="logo-main-1"
+    src="https://cdn.discordapp.com/icons/878341201399521351/30a501ea20ba724492fc6b9a90844f8f.webp?size=1024"
+  />
+
+  <p id="botname-main-1">Re4e Bot</p>
+
+  <div id="main-buttons">
+    <a href="https://discordbotlist.com/bots/re4e"
+      ><button id="main-button-1">VOTE</button></a
+    >
+
+   <a href="https://discord.gg/JGkbyRaU9S"><button id="main-button-2">Support Server</button></a
+  </div>
+     
+
+     
+  <div style="margin:50px 0 50px 0;"></div>
+
+  <div id="main-info">
+    <br id="intro" />
+
+    <p id="main-info-1">
+
+
+
+    <p id="main-info-2"></p>
+  </div>
+
+  <!--                       WEBSITE BY Romeo#0700-->
+
+    >
+  </div>
+
+  <div id="main-buttons-2" style="margin-top: 30px">
+    <a href="https://twitter.com/Re4eBot"><button id="main-button-3">Twitter</button></a>
+
+    <a href="https://streamlabs.com/re4ebot/tip"><button id="main-button-4">Donate</button></a>
+  </div>
+</body>
+
+<footer>
+  <p id="footer-copyright"></p>
+
+  <a href=""><p id="footer-credits"></p></a>
+</footer>
+
+<script src="js/script.js"></script>
